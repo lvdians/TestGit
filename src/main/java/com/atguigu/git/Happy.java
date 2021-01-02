4dc2ef6826a70c69abda1207aa0b4726f8fc0bb4
vv <vv>
2021-01-02 15:45:04 +0800

    你真好

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -2,10 +2,8 @@ package com.atguigu.git;
 
 public class Happy {
 public static void main(String[] args) {
-<<<<<<< HEAD
 	System.out.println("right...");
-=======
 	System.out.println("left...");
->>>>>>> branch 'master' of https://github.com/lvdians/TestGit.git
+	System.out.print("你为什么修改");
 }
 }
